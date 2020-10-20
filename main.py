355ce34292c0b8c61fb483848a44b559c4fb1b26
Holthuizen <45522614+Holthuizen@users.noreply.github.com>
2020-10-21 00:30:02 +0200

    improve readability and adding two diferent key stream options
    split PRGA in two methords

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 class RC4: 
     def __init__(self,key): 
-       self.key =  self.PRGA( self.KSA( list(key.encode() ) ))#return generator that constantly updates the secret internal state S
+       #returns list of ints of given length
+       self.key =  self.PRGA( self.KSA(list(key.encode())),10)
+       
+       #returns generator that can generate one word of the keystream a time without given limit
+       self.keygenerator =  self.PRGA_YIELD( self.KSA(list(key.encode())))
+
     def KSA(self,key):
-        s = list(range(0,256))#internal state (array from 0 to 255 )
+        s = list(range(0,256))#internal state, array [0 - 255] 
         j = 0
         for i in range(256):
             j = (j+s[i]+key[i%len(key)])% 256
             s[i],s[j] = s[j],s[i] #list swap
         return s
-    def PRGA(self,S):
+    
+    def PRGA_YIELD(self,S):
         i,j = 0,0
         while True:
             i = (i + 1) % 256
             j = (j + S[i]) % 256
             S[i], S[j] = S[j], S[i]  # swap
-
             K = S[(S[i] + S[j]) % 256]
             yield K
+
+    def PRGA(self,S,n):
+        i,j = 0,0
+        key=[]
+        for i in (range(n)):
+            i = (i + 1) % 256
+            j = (j + S[i]) % 256
+            S[i], S[j] = S[j], S[i]  # swap
+            key.append( S[(S[i] + S[j]) % 256] )
+        return key
 
 # Test vectors  https://en.wikipedia.org/wiki/RC4 :
 
@@ -32,12 +47,19 @@ streamcipher = RC4("Key")
 # Key = Secret 	
 # Keysteam = 04D46B053CA87B59 	
 
-
-keystream = streamcipher.key
+#get keystream generator
+keystream = streamcipher.keygenerator
 #format integers to hex for comperason. 
 for c in range(10):
     print("%02X" % (next(keystream)),end='')
 
 
+print("",end='\n')
+
+#get keystream. 
+keystream = streamcipher.key
+#format integers to hex for comperason. 
+for c in keystream:
+    print("%02X" % (c),end='')
 
 
